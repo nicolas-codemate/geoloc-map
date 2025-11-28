d68e1de2294ae2f0b06fc78c0fe056e0991e3fb4
Nicolas <nicolas@codemate.consulting>
2025-11-28 15:23:00 +0100

    Fix map UX: use early hint to prevent desynchro between markers and popup

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -8,16 +8,29 @@ use App\Exception\MapConfigNotFoundException;
 use App\Service\MapConfigBuilder;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Asset\Packages;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\WebLink\Link;
 use Twig\Error\RuntimeError;
 
 class MapController extends AbstractController
 {
     #[Route('/{mapName}', name: 'map')]
-    public function __invoke(MapConfigBuilder $configBuilder, string $mapName, LoggerInterface $logger, Request $request): Response
-    {
+    public function __invoke(
+        MapConfigBuilder $configBuilder,
+        string $mapName,
+        LoggerInterface $logger,
+        Request $request,
+        Packages $assetPackages,
+    ): Response {
+        // Send Early Hints (HTTP 103) to preload critical Leaflet assets
+        $this->sendEarlyHints([
+            (new Link('preload', $assetPackages->getUrl('vendor/leaflet/dist/leaflet.min.css')))->withAttribute('as', 'style'),
+            (new Link('preload', $assetPackages->getUrl('vendor/leaflet/dist/images/marker-icon.png')))->withAttribute('as', 'image'),
+        ]);
+
         try {
             return $this->render('map.html.twig', [
                 'mapName' => $mapName,
